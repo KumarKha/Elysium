82162671f49c7fe5571261ccc031d109617bd87e
Kumar Khalikaprasad <kumar@Kumars-MacBook-Pro.local>
2026-03-31 12:07:31 -0400

    Initial project setup: Spring Boot server + React client

---
+++ server/src/main/java/com/elysium/server/ElysiumServer.java
@@ -0,0 +1,17 @@
+package com.elysium.server;
+
+/**
+ * Hello world!
+ *
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElysiumServer {
+  public static void main(String[] args) {
+    SpringApplication.run(ElysiumServer.class, args);
+
+  }
+}
---
+++ server/src/main/java/com/elysium/server/models/Booking.java
@@ -0,0 +1,67 @@
+package com.elysium.server.models;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.*;
+
+public class Booking {
+  private String customerFirstName;
+  private String customerLastName;
+
+  private LocalDateTime requestedBookingTime;
+  @CreationTimestamp
+  private LocalDateTime bookingCreationDate;
+  @UpdateTimestamp
+  private LocalDateTime bookingUpdateDate;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long bookingID;
+
+  public enum Status {
+    OPEN,
+    CLOSED,
+    PENDING
+  }
+  @Enumerated(EnumType.STRING)
+  private Status bookingStatus;
+
+  public Booking() {
+  }
+
+  public void setCustomerFirstName(String firstName) {
+    this.customerFirstName = firstName;
+  }
+
+  public void setCustomerLastName(String lastName) {
+    this.customerLastName = lastName;
+  }
+
+  public void setBookingTime(LocalDateTime bookingTime) {
+    this.requestedBookingTime = bookingTime;
+  }
+
+  public void setBookingStatus(String status) {
+    try {
+      this.bookingStatus = Status.valueOf(status.toUpperCase());
+    } catch (IllegalArgumentException | NullPointerException e) {
+      throw new IllegalArgumentException(
+          "Invalid status. Must be one of: OPEN, CLOSED, PENDING");
+    }
+  }
+
+  public String getCustomerFirstName() {
+    return customerFirstName;
+  }
+
+  public String getCustomerLastName() {
+    return customerLastName;
+  }
+  public LocalDateTime getBookingTime(){
+    return requestedBookingTime;
+  }
+  public 
+
+}
